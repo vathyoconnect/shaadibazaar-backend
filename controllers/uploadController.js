4026805cdb69c232869e21e58cc5faaf36a2bd25
vathyoconnect <vathyoconnect@gmail.com>
2025-05-14 15:25:10 -0400

    Delete controllers directory

--- controllers/uploadController.js
+++
@@ -1,6 +0,0 @@
-exports.uploadImage = (req, res) => {
-    if (!req.file) {
-        return res.status(400).json({ message: 'No file uploaded' });
-    }
-    res.status(200).json({ filename: req.file.filename, path: `/uploads/${req.file.filename}` });
-};
